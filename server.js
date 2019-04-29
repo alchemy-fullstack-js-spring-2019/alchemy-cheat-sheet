042d283a40bd59e2c95e0edaa20b5bc525581122
Leslie Shea <leslieyshea@gmail.com>
2019-04-29 10:24:34 -0700

    travis file setup/server setup

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('hello');
+require('dotenv').config();
+require('./lib/utils/connect');
+const app = require('./lib/app');
+const PORT = process.env.PORT || 3355;
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}.`);
+});
+
