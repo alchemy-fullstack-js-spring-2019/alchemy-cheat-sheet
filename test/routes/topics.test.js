9541a5402b1d4a8abd42c5e313fd03c445556585
Leslie Shea <leslieyshea@gmail.com>
2019-04-29 14:46:26 -0700

    can get a list of topics test passing

--- test/routes/topics.test.js
+++ test/routes/topics.test.js
@@ -3,21 +3,29 @@ const request = require('supertest');
 const app = require('../../lib/app');
 const Topic = require('../../lib/models/Topic');
 
+const { getTopics } = require('../data-helpers');
+
 describe('topics routes', () => {
   it('can create a new topic', async() => {
-    return request(app)
+    const topic = await request(app)
       .post('/api/v1/topics')
       .send({
         title: 'MongoDB',
         description: 'Notes on MongoDB'
-      })
-      .then(res => {
-        expect(res.body).toEqual({
-          title: 'MongoDB',
-          description: 'Notes on MongoDB',
-          _id: expect.any(String),
-          __v: 0
-        });
       });
+    expect(topic.body).toEqual({
+      title: 'MongoDB',
+      description: 'Notes on MongoDB',
+      _id: expect.any(String),
+      __v: 0
+    });
+  });
+  
+  it('can find all topics', async() => {
+    await getTopics();
+    const topics = await request(app)
+      .get('/api/v1/topics');
+    
+    expect(topics.body).toHaveLength(10);
   });
 });
