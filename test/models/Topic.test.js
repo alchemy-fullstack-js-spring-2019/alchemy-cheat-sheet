149a3961c4ce73da9b916d9b0a8036135f881011
Leslie Shea <leslieyshea@gmail.com>
2019-04-29 11:42:41 -0700

    updated to title and description, requires title test passing

--- test/models/Topic.test.js
+++ test/models/Topic.test.js
@@ -2,16 +2,22 @@ const mongoose = require('mongoose');
 const Topic = require('../../lib/models/Topic');
 
 describe('Topic model', () => {
-  it('has a title and notes', () => {
+  it('has a title and description', () => {
     const topic = new Topic({
       title: 'Express',
-      notes: [new mongoose.Types.ObjectId()]
+      description: 'All notes about Express'
     });
 
     expect(topic.toJSON()).toEqual({
       title: 'Express',
-      notes: expect.any(Array),
+      description: 'All notes about Express',
       _id: expect.any(mongoose.Types.ObjectId)
     });
+  });
+  it('requires a title', () => {
+    const topic = new Topic({});
+
+    const errors = topic.validateSync().errors;
+    expect(errors.title.message).toEqual('Path `title` is required.');
   });
 });
