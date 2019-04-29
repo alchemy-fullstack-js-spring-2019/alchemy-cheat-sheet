0c66cc0cc51ec2df9ddeda9cb547fadb7b6e5e03
Leslie Shea <leslieyshea@gmail.com>
2019-04-29 14:21:51 -0700

    added seed data/data helpers

---
+++ test/data-helpers.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+const seedData  = require('./seed-data');
+// const request = require('supertest');
+// const app = require('../lib/app');
+const Topic = require('../lib/models/Topic'); 
+// const agent = request.agent(app);
+
+beforeAll(async() => {
+  await connect();
+});
+
+beforeEach(async() => {
+  await mongoose.connection.dropDatabase();
+//   return agent
+//     .post('/api/v1/auth/signup')
+//     .send({ email:'email', password:'123' });
+});
+
+beforeEach(async() => {
+  await seedData();
+});
+
+afterAll(async() => {
+  await mongoose.connection.close();
+});
+
+const prepare = model => JSON.parse(JSON.stringify(model));
+const createGetters = Model => ({
+  [`get${Model.modelName}`]: query => Model.findOne(query).then(prepare),
+  [`get${Model.modelName}s`]: query => Model.find(query).then(models => models.map(prepare))
+});
+
+module.exports = {
+  ...createGetters(Topic)
+  // getAgent: () => agent
+};
---
+++ test/seed-data.js
@@ -0,0 +1,12 @@
+const chance = require('chance').Chance();
+const Topic = require('../lib/models/Topic');
+
+
+module.exports = ({ topicCount = 10 } = {}) => {
+  const topics = [...Array(topicCount)].map(() => ({
+    title: chance.name(),
+    description: chance.word({ length: 5 })
+  }));
+
+  Topic.create(topics); 
+};
