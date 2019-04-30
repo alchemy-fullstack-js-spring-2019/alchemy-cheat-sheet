b65fbc19eb5bf7d1a479535c31a0df5a9f8e74ec
Leslie Shea <leslieyshea@gmail.com>
2019-04-30 11:57:27 -0700

    can sign up a user route in progress

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,1 +1,38 @@
+require('dotenv').config();
+const request = require('supertest');
+const app = require('../../lib/app');
+const mongoose = require('mongoose');
+const connect = require('../../lib/utils/connect');
+const User = require('../../lib/models/User');
 
+
+describe('auth routes', () => {
+  beforeAll(async() => {
+    await connect();
+  });
+
+  beforeEach(async() => {
+    await mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+  });
+  
+  it('can sign up a user', async() => {
+    const user = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({ 
+        username: 'testname', 
+        password: '123',
+        role: 'contributor' 
+      });
+    expect(user.body).toEqual({
+      user: {
+        username: 'testnuuser',
+        role: 'contributor',
+        _id: expect.any(String)
+      }
+    });
+  });
+});
