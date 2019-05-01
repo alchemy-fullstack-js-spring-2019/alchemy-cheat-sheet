d640bd967d2bfbff0660be5ed4a5c177ca1e0e03
Leslie Shea <leslieyshea@gmail.com>
2019-05-01 15:45:37 -0700

    can exit or go back to topics list

--- client/handlers/home-page.js
+++ client/handlers/home-page.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const { get } = require('../commons/request');
 
-module.exports = async() =>  {
+const homePageHandler = async() =>  {
   const topics = await get('/topics');
   const topicsList = topics.body.map(topic => {
     return { name: topic.title, value: topic._id };
@@ -13,28 +13,46 @@ module.exports = async() =>  {
     choices: topicsList
   };
 
-  inquirer.prompt([
+  const chosenTopic = await inquirer.prompt([
     chooseTopic
-  ])
-    .then(async(chosenTopic) => {
-      const notes = await get(`/topics/notes/${chosenTopic.Topic}`);
-      const notesList = notes.body.map(note => {
-        return { name: note.title, value: note._id };
-      });
+  ]);
+  const notes = await get(`/topics/notes/${chosenTopic.Topic}`);
+  const notesList = notes.body.map(note => {
+    return { name: note.title, value: note._id };
+  });
 
-      const chooseNote = {
-        type: 'list',
-        name: 'Note',
-        choices: notesList
-      };
+  const chooseNote = {
+    type: 'list',
+    name: 'Note',
+    choices: notesList,
+  };
 
-      inquirer.prompt([
-        chooseNote
-      ])
-        .then(async(chosenNote) => {
-          const note = await get(`/notes/${chosenNote.Note}`);
-          console.log('Content:', note.body.content);
-          console.log('Last Updated:', note.body.updatedAt);
-        });
-    });
+  const chosenNote = await inquirer.prompt([
+    chooseNote
+  ]);
+  const note = await get(`/notes/${chosenNote.Note}`);
+  console.log('Content:', note.body.content);
+  console.log('Last Updated:', note.body.updatedAt);
+
+  const exitOptions = [
+    { name: 'Exit to console', value: 'exit' },
+    { name: 'Back to topics', value: 'rerun' }
+  ];
+
+  const chooseExit = {
+    type: 'list',
+    name: 'Choice',
+    choices: exitOptions,
+  };
+
+  const chosenExit = await inquirer.prompt([
+    chooseExit
+  ]);
+  if(chosenExit.Choice === 'exit') {
+    console.log('Thanks for using Alchemy Cheat Sheets!');
+  } else {
+    return homePageHandler();
+  }
 };
+
+module.exports = homePageHandler;
