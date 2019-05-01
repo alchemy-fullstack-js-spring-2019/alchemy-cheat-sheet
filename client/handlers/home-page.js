dea0777a5fab7767281dcb0e3e251b24198bacfd
Leslie Shea <leslieyshea@gmail.com>
2019-05-01 11:25:52 -0700

    trying to get topics

--- client/handlers/home-page.js
+++ client/handlers/home-page.js
@@ -3,7 +3,25 @@ const { get } = require('../commons/request');
 const request = require('superagent');
 
 //module.exports = async() => { await get('/topics'); };
-const test = get('/topics');
-console.log(test.body);
 
+let notes = [];
+const test = async()=>  await get('/topics')
+  .then(result=>{console.log(result.body);
+    
+    notes = result.body;
+  });
+test();
+// let test2;
+// async()=>{
+//   test2 = await get('/topics');
+// };
+
+try {
+  // console.log('test2', test2);
+  // console.log('tryuign to run test');
+  console.log(notes);
+  
+} catch(error){
+  console.log(error);
+}
 
